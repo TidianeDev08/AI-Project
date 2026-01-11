48825306b0e3d0f1800d82ba1e4457959d61c27a
TidianeDev08 <tidiane1787@gmail.com>
2026-01-11 19:21:28 +0300

    creation of the first artificial neuron

---
+++ AI.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+X, y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
+y = y.reshape(y.shape[0], 1)
+
+print('dimensions de X:', X.shape)
+print('dimensions de y:', y.shape)
+
+plt.scatter(X[:,0],X[:, 1], c=y, cmap='summer')
+plt.show()
+
+def initialisation(X):
+    W = np.random.randn(X.shape[1], 1)
+    b = np.random.randn(1)
+    return W, b
+
+def model(X, W, b):
+    Z = np.dot(W) + b
+    A = 1/(1 + np.exp(-Z))
+    return A
+
+def log_loss(A, y):
+    return 1/len(y) * np.sum(-y * np.log(A) - (1 - y) * np.log(1 - A))
